2d67db39c2c399d49bc316436705477e83e0e3c2
x3imal <aasurov@gmail.com>
2023-02-13 18:06:20 +0300

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
-}
+
+
+        public static void task1 () {
+            int age = 19;
+            if (age >= 18) {
+                System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+            }
+            if (age < 18) {
+                System.out.println("Если возраст человека равен " + age + ", то он несовершеннолетний.");
+            }
+        }
+    }
